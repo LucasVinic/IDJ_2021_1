0cd143b02e49d37687340812b8bc32f1719247ee
Lucas <lucasvini1298@gmail.com>
2021-09-01 18:15:57 -0300

    Inicializa repo - pt2

---
+++ include/Game.h
@@ -0,0 +1,29 @@
+#ifndef game
+#define game
+#include "SDL.h"
+#include <string>
+
+using namespace std;
+
+class Game {
+  private: 
+    Game (
+      string title,
+      int width,
+      int height
+    );
+    static Game* instance;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    /* State* state; */
+
+  public: 
+    ~Game ();
+    void Run();
+    SDL_Renderer* GetRenderer();
+    /* State& GetState(); */
+    static Game& GetInstance();
+
+
+};
+#endif
